e0974c5e8ed1bc5158bea1f8b247c2ccfa6f05d6
dskleingeld <11743287+dskleingeld@users.noreply.github.com>
2020-12-02 22:29:31 +0100

    streaming working for mp3

--- src/play.rs
+++ src/play.rs
@@ -1,9 +1,9 @@
 // use futures::Stream;
 use std::sync::mpsc;
-use reqwest::RequestBuilder;
-use rodio::{Source, Sample};
-use minimp3::{Decoder, Frame};
-use std::io::{self, SeekFrom};
+use bytes::Bytes;
+use std::io::{self, Seek, SeekFrom, Read, ErrorKind};
+use rodio::Decoder;
+use eyre::WrapErr;
 
 /* design:
  * implement a readable storage that grows via appending trough an 
@@ -12,37 +12,6 @@ use std::io::{self, SeekFrom};
  * rodio::decoder::Decoder from which we build a rodio::Source. That
  * is the played using rodio.
  */
-
-// decode and provide 
-// stream for rodio
-struct Mp3Stream {
-    rx: mpsc::Receiver<bytes::Bytes>,
-    decoder: Decoder<bytes::Bytes>,
-    current_frame: Frame,
-    current_frame_offset: usize,
-}
-
-// impl Source for Mp3Stream {
-//     fn current_frame_len(&self) -> Option<usize> {
-//         None
-//     }
-//     fn channels(&self) -> u16 {
-//         1
-//     }
-//     fn sample_rate(&self) -> u32 {
-        
-//     }
-//     fn total_duration(&self) -> Option<std::time::Duration> {
-//         None
-//     }
-// }
-
-// impl Iterator<Item = Sample> for Mp3Stream {
-//     fn next(&mut self) -> Option<Self::Item> {
-        
-//     }
-
-// }
 
 struct ReadableReciever {
     rx: mpsc::Receiver<bytes::Bytes>,
@@ -61,7 +30,7 @@ impl ReadableReciever {
     }
 }
 
-impl std::io::Read for ReadableReciever {
+impl Read for ReadableReciever {
     fn read(&mut self, buf: &mut [u8]) -> Result<usize, io::Error> {
         let needed = buf.len();
         let mut unread_buffer = self.buffer.len() - self.offset;
@@ -72,7 +41,9 @@ impl std::io::Read for ReadableReciever {
         }
 
         // get extra bytes, and put them in the buffer
-        let bytes = self.rx.recv().unwrap();
+        // if no bytes are gotten this or the next call to read
+        // will return 0 indicating end of file
+        let bytes = self.rx.recv().unwrap_or(bytes::Bytes::new());
         self.buffer.extend_from_slice(&bytes);
         unread_buffer += bytes.len();
         
@@ -82,7 +53,7 @@ impl std::io::Read for ReadableReciever {
             needed
         } else {
             // less bytes then needed, return what we got do not block
-            buf[..unread_buffer].clone_from_slice(&self.buffer[self.offset..self.offset+needed]);
+            buf[..unread_buffer].clone_from_slice(&self.buffer[self.offset..self.offset+unread_buffer]);
             unread_buffer
         };
         self.offset += read;
@@ -92,40 +63,124 @@ impl std::io::Read for ReadableReciever {
 
 impl std::io::Seek for ReadableReciever {
     fn seek(&mut self, pos: SeekFrom) -> io::Result<u64> {
-        todo!()        
+        use SeekFrom::*;
+        match pos {
+            Current(p) if self.offset as i64 +p > self.buffer.len() as i64 => 
+                Err(io::Error::new(ErrorKind::UnexpectedEof, "cannot seek after end of reader")),
+            Current(p) if self.offset as i64 +p < 0 => 
+                Err(io::Error::new(ErrorKind::UnexpectedEof, "cannot seek before start of reader")),
+            Start(p) if p > self.buffer.len() as u64 =>
+                Err(io::Error::new(ErrorKind::UnexpectedEof, "cannot seek after end of reader")),
+            End(p) if self.buffer.len() as i64 + p < 0 =>
+                Err(io::Error::new(ErrorKind::UnexpectedEof, "cannot seek before start of reader")),
+            End(p) if p > 0 =>
+                Err(io::Error::new(ErrorKind::UnexpectedEof, "cannot seek after end of reader")),
+
+            Start(p) => {self.offset = p as usize; Ok(self.offset as u64)}
+            Current(p) => {self.offset = (self.offset as i64 + p) as usize; Ok(self.offset as u64)}
+            End(p) => {self.offset = (self.offset as i64 + p) as usize; Ok(self.offset as u64)}
+        }
     }
 }
 
-// type HttpStream = reqwest::Response;
-// async fn start_streaming(url: &str) -> eyre::Result<(Mp3Stream, HttpStream)> {
-//     let (tx, rx) = mpsc::channel();
-//     let mut decoder = Decoder::new(data); //TODO replace data with something that implements read
-//     let current_frame = decoder.next_frame().unwrap()?;
+#[test]
+fn test_readable_reciever_seek_read_exact() {
+    let (tx, rx) = mpsc::channel();
+    let mut readable_rx = ReadableReciever::new(rx);
+    const T1: &str = "Hello world!";
+    const T2: &str = " are you getting this";
 
-//     let mut res = reqwest::get(url).await?;
-//     while let Some(chunk) = res.chunk().await? {
-//         tx.send(chunk);
-//     }
+    let t1 = Bytes::from(T1);
+    let t2 = Bytes::from(T2);
+    tx.send(t1).unwrap();
 
-//     let stream = Mp3Stream {
-//         decoder,
-//         current_frame,
-//         current_frame_offset = 0,
-//     };
-// }
+    let mut buffer = vec![0;T1.len()];
+    readable_rx.read_exact(&mut buffer).unwrap();
+    assert_eq!(T1.as_bytes(), buffer);
 
-// async fn continue_streaming(stream: HttpStream){
-//     let (tx, rx) = mpsc::channel();
-//     let stream = Mp3Stream {
-//         rx,
-//     };
+    tx.send(t2).unwrap();
+    readable_rx.seek(SeekFrom::Start(0)).unwrap();
+    let mut buffer = vec![0;T1.len()+T2.len()];
+    readable_rx.read_exact(&mut buffer).unwrap();
+    assert_eq!([T1,T2].concat().as_bytes(), buffer);
+}
 
-//     while let Some(chunk) = stream.chunk().await? {
-//         tx.send(chunk);
-//     }
-// }
+#[test]
+fn test_readable_reciever_seek_read_string_eof() {
+    use std::thread;
 
-// separate thread for streaming decode? (can be done async)
-// source will be moved to audiothread? 
-// audio playback is already done by a separate thread
-// need way to send back errors from the audiothread... can we?
+    let (tx, rx) = mpsc::channel();
+    let mut readable_rx = ReadableReciever::new(rx);
+    const T1: &str = "Hello world!";
+    const T2: &str = " are you getting this";
+
+    let t1 = Bytes::from(T1);
+    tx.send(t1).unwrap();
+
+    let child = thread::spawn(move || {
+        let mut buffer = String::new();
+        readable_rx.read_to_string(&mut buffer).unwrap();
+        assert_eq!([T1,T2].concat(), buffer);
+    });
+
+    let t2 = Bytes::from(T2);
+    tx.send(t2).unwrap();
+
+    drop(tx); // indicates the end (EOF)
+    // only now the child thread can read to end of file
+    child.join().unwrap();
+}
+
+struct WebToDecoderStream {
+    res: reqwest::Response,
+    tx: mpsc::Sender<Bytes>,
+}
+
+// TODO support more then mp3 if needed [are podcasts always mp3?]
+async fn start_streaming(url: &str) -> eyre::Result<(Decoder<ReadableReciever>, WebToDecoderStream)> {
+    let (tx, rx) = mpsc::channel();
+    let readable_rx = ReadableReciever::new(rx);
+    dbg!();
+    let mut recieved = 0;
+    let mut res = reqwest::get(url).await?;
+    while recieved < 32_000 {
+        // get some data into readable_rx. Otherwise  creating the 
+        // decoder will fail as it has no header data 
+        if let Some(chunk) = res.chunk().await.unwrap() {
+            dbg!();
+            recieved += chunk.len();
+            tx.send(chunk).unwrap();
+        }
+    }
+    dbg!();
+    let decoder = Decoder::new_mp3(readable_rx).unwrap();
+    dbg!();
+    Ok((decoder, WebToDecoderStream{res, tx}))
+}
+
+async fn continue_streaming(stream: WebToDecoderStream) -> eyre::Result<()> {
+    let WebToDecoderStream {mut res, tx} = stream;
+    while let Some(chunk) = res.chunk().await.wrap_err("stream failed")? {
+        dbg!(chunk.len());
+        tx.send(chunk).unwrap();
+    }
+    Ok(())
+}
+
+#[test]
+fn stream_mp3() {
+    use tokio::runtime::Runtime;
+    const URL: &str = "http://icecast.omroep.nl/radio2-bb-mp3";
+
+    // Create the runtime
+    Runtime::new()
+        .unwrap()
+        .block_on(async {
+            let (source, passer) = start_streaming(URL).await.unwrap();
+            let (stream, stream_handle) = rodio::OutputStream::try_default().unwrap();
+            let sink = rodio::Sink::try_new(&stream_handle).unwrap();
+            sink.append(source);
+            continue_streaming(passer).await.unwrap();
+        });
+
+}
